b86be51c6130dab5bf1721137726585575ba8dbd
Kayode Olaniyi <ifedamilola2009@gmail.com>
2024-06-27 15:12:18 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+git pull
 rm -r node_modules/
 npm install
 node ace migration:run
@@ -7,3 +8,4 @@ npm ci --omit="dev"
 pm2 stop server 
 # ENV_PATH=$ENV_PATH pm2 start bin/server.js
 cp ../.env .env
+pm2 start bin/server.js
