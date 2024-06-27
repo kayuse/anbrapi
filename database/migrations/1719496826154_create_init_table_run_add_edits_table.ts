d71dfd0faac058d7089c53dbe3b47b6fee398768
Kayode Olaniyi <ifedamilola2009@gmail.com>
2024-06-27 15:06:37 +0100

    changed
    changed

--- database/migrations/1719496826154_create_init_table_run_add_edits_table.ts
+++ database/migrations/1719496826154_create_init_table_run_add_edits_table.ts
@@ -2,16 +2,22 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
   protected tableName = 'registrations'
-
+  protected bibleStudyGroups = 'bible_study_groups'
   async up() {
     this.schema.alterTable(this.tableName, (table) => {
       table.string('your_description')
+    })
+    this.schema.alterTable(this.bibleStudyGroups, (table) => {
+      table.integer('attendant')
     })
   }
 
   async down() {
     this.schema.alterTable(this.tableName, (table) => {
       table.dropColumn('your_description')
+    })
+    this.schema.alterTable(this.bibleStudyGroups, (table) => {
+      table.dropColumn('attendant')
     })
   }
 }
